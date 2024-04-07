e2cd060530d3c62af677eabc7205c0a283a325eb
OpalinskiJakub <jakopal1125@gmail.com>
2024-04-08 01:38:02 +0200

    working cmakefile

---
+++ cmake-build-debug/CMakeFiles/Unpacker2.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/jakub/CLionProjects/Unpacker2/src/EventIII.cc" "CMakeFiles/Unpacker2.dir/src/EventIII.cc.o" "gcc" "CMakeFiles/Unpacker2.dir/src/EventIII.cc.o.d"
+  "/home/jakub/CLionProjects/Unpacker2/src/TDCChannel.cc" "CMakeFiles/Unpacker2.dir/src/TDCChannel.cc.o" "gcc" "CMakeFiles/Unpacker2.dir/src/TDCChannel.cc.o.d"
+  "/home/jakub/CLionProjects/Unpacker2/src/Unpacker2.cc" "CMakeFiles/Unpacker2.dir/src/Unpacker2.cc.o" "gcc" "CMakeFiles/Unpacker2.dir/src/Unpacker2.cc.o.d"
+  "/home/jakub/CLionProjects/Unpacker2/src/Unpacker2D.cc" "CMakeFiles/Unpacker2.dir/src/Unpacker2D.cc.o" "gcc" "CMakeFiles/Unpacker2.dir/src/Unpacker2D.cc.o.d"
+  "/home/jakub/CLionProjects/Unpacker2/src/main.cpp" "CMakeFiles/Unpacker2.dir/src/main.cpp.o" "gcc" "CMakeFiles/Unpacker2.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cmake-build-debug/CMakeFiles/Unpacker2.dir/cmake_clean.cmake
@@ -0,0 +1,19 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Unpacker2.dir/src/EventIII.cc.o"
+  "CMakeFiles/Unpacker2.dir/src/EventIII.cc.o.d"
+  "CMakeFiles/Unpacker2.dir/src/TDCChannel.cc.o"
+  "CMakeFiles/Unpacker2.dir/src/TDCChannel.cc.o.d"
+  "CMakeFiles/Unpacker2.dir/src/Unpacker2.cc.o"
+  "CMakeFiles/Unpacker2.dir/src/Unpacker2.cc.o.d"
+  "CMakeFiles/Unpacker2.dir/src/Unpacker2D.cc.o"
+  "CMakeFiles/Unpacker2.dir/src/Unpacker2D.cc.o.d"
+  "CMakeFiles/Unpacker2.dir/src/main.cpp.o"
+  "CMakeFiles/Unpacker2.dir/src/main.cpp.o.d"
+  "Unpacker2"
+  "Unpacker2.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Unpacker2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
